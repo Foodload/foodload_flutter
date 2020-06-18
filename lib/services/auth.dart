513a09262146f88dcc2156f1047bbd2968331b2a
Gurk1992 <Gurk1992@users.noreply.github.com>
2020-06-18 15:56:38 +0200

    changed url

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -52,18 +52,21 @@ class AuthService {
 
   /*Send X Req to REST API, prob refactor this soon*/
   Future<String> sendTokenToRest() async {
-    final url = "REST-URL"; //fill this
+    final url = 'https://foodload.herokuapp.com/login'; //fill this
     var token = await extractToken();
     Map<String, String> headers = {
       'Content-type': 'application/json',
       'Authorization': 'Bearer $token',
     };
+
     //print(headers['Authorization']);
     Response resp = await get(url, headers: headers);
     int statusCode = resp.statusCode;
+    print(statusCode);
     if (statusCode != 200) {
       return "Did not receive 200";
     }
-    return resp.body.toString();
+    print(resp.body);
+    return resp.body;
   }
 }
