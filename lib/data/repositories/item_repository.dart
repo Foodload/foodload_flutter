80e59d81b1450bfb06c9ff460c45dba701bee961
Antonio <antonio_97@live.se>
2020-07-23 22:31:34 +0200

    added get item function

--- lib/data/repositories/item_repository.dart
+++ lib/data/repositories/item_repository.dart
@@ -1,5 +1,6 @@
 import 'package:foodload_flutter/data/providers/foodload_api_client.dart';
 import 'package:foodload_flutter/models/item.dart';
+import 'package:foodload_flutter/models/item_info.dart';
 import 'package:meta/meta.dart';
 
 class ItemRepository {
@@ -39,5 +40,21 @@ class ItemRepository {
         amount: 3,
       ),
     ];
+  }
+
+  Future<ItemInfo> getItem(String id) async {
+    //api stuff
+    List<ItemInfo> itemsDummy = [
+      ItemInfo(
+        id: '1',
+        title: 'Milk',
+      ),
+      ItemInfo(
+        id: '2',
+        title: 'Bread',
+      ),
+      ItemInfo(id: '3', title: 'Yoghurt'),
+    ];
+    return itemsDummy.firstWhere((itemInfo) => itemInfo.id == id, orElse: null);
   }
 }
