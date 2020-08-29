5d8c3f9c953d3e2a1bb7bacb3cd01adc14a72fc5
Antonio <antonio_97@live.se>
2020-08-29 17:49:50 +0200

    moved appdrawer to the top of the scaffold

--- lib/ui/screens/landing_screen.dart
+++ lib/ui/screens/landing_screen.dart
@@ -53,55 +53,56 @@ class LandingScreen extends StatelessWidget {
     final _mediaQuery = MediaQuery.of(context);
     final _isLandscape = _mediaQuery.orientation == Orientation.landscape;
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text(storages_title),
-          actions: <Widget>[
-            DropdownButton(
-              underline: Container(),
-              icon: const Icon(Icons.more_vert),
-              items: [
-                DropdownMenuItem(
-                  child: Container(
-                    child: Row(
-                      children: <Widget>[
-                        const Icon(Icons.exit_to_app),
-                        const SizedBox(width: 8),
-                        const Text('Logout'),
-                      ],
-                    ),
+      resizeToAvoidBottomInset: false,
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: const Text(storages_title),
+        actions: <Widget>[
+          DropdownButton(
+            underline: Container(),
+            icon: const Icon(Icons.more_vert),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      const Icon(Icons.exit_to_app),
+                      const SizedBox(width: 8),
+                      const Text('Logout'),
+                    ],
                   ),
-                  value: 'logout',
                 ),
-                DropdownMenuItem(
-                  child: Container(
-                    child: const Text('Test'),
-                  ),
-                  value: 'test',
+                value: 'logout',
+              ),
+              DropdownMenuItem(
+                child: Container(
+                  child: const Text('Test'),
                 ),
-              ],
-              onChanged: (val) {
-                switch (val) {
-                  case 'logout':
-                    //Navigator.of(context).pushReplacementNamed('/'); //if logout should be possible from wherever
-                    BlocProvider.of<AuthBloc>(context).add(
-                      AuthLoggedOut(),
-                    );
-                    break;
-                  case 'test':
-                    Navigator.of(context).pushNamed(TestScreen.routeName);
-                    break;
-                  default:
-                    print(val);
-                }
-              },
-            ),
-          ],
-        ),
-        body: Center(
-          child:
-              _isLandscape ? _buildLandscapeContent() : _buildPortraitContent(),
-        ),
-        drawer: AppDrawer());
+                value: 'test',
+              ),
+            ],
+            onChanged: (val) {
+              switch (val) {
+                case 'logout':
+                  //Navigator.of(context).pushReplacementNamed('/'); //if logout should be possible from wherever
+                  BlocProvider.of<AuthBloc>(context).add(
+                    AuthLoggedOut(),
+                  );
+                  break;
+                case 'test':
+                  Navigator.of(context).pushNamed(TestScreen.routeName);
+                  break;
+                default:
+                  print(val);
+              }
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child:
+            _isLandscape ? _buildLandscapeContent() : _buildPortraitContent(),
+      ),
+    );
   }
 }
